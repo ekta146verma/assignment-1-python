a0d65bc668a69338764ec57415a62b519a53a47b
Ekta Verma <92796078+ekta146verma@users.noreply.github.com>
2022-09-27 22:49:20 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,35 @@
+a=int(input("enter the value of a"))
+print(a)
+b=int(input("enter the value of b"))
+print(b)
+op=str(input("Enter the operation you want to perform"))
+print(op)
+if(op=='+'):
+    {
+    print(a+b)
+
+    }
+elif(op=='-'):
+    {
+    print(a-b)
+
+    }
+elif(op=='*'):
+    {
+    print(a*b)
+    
+    }
+elif(op=='/'):
+    {
+    print(a/b)
+    
+    }
+elif(op=='%'):
+    {
+    print(a%b)
+    
+    }
+else:
+    print("enter the valid operation")
+    
+
